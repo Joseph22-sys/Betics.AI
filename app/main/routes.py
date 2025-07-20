7704d0b85f25099e830277091391a44ffde2a1f6
Ekwenye Joseph <ekwenyejoseph9@gmail.com>
2025-07-20 07:49:54 +0100

    fixing the issue with dashboard rendering

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,9 @@
 from app.main import bp
-from flask import render_template
+from flask import render_template,session
+
+
 
 @bp.route('/')
 def home():
+    session.clear()
     return render_template("home.html")
